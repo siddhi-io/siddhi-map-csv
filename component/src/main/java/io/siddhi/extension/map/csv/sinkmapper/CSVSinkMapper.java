05d4bb0e5ab9b836127da6e06f2fe1a2f93a2669
lasanthaS <lasn1987@gmail.com>
2022-11-22 12:43:27 +0530

    Fix sink mapper for TSV

--- component/src/main/java/io/siddhi/extension/map/csv/sinkmapper/CSVSinkMapper.java
+++ component/src/main/java/io/siddhi/extension/map/csv/sinkmapper/CSVSinkMapper.java
@@ -119,6 +119,8 @@ public class CSVSinkMapper extends SinkMapper {
     private static final String DEFAULT_GROUP_EVENTS = "false";
     private static final String DEFAULT_HEADER = "false";
     private static final String DEFAULT_DELIMITER = ",";
+    private static final String TAB_DElIMITER_INPUT = "\\t";
+    private static final char TAB_DElIMITER = '\t';
 
     private StreamDefinition streamDefinition;
     private boolean eventGroupEnabled;
@@ -160,7 +162,7 @@ public class CSVSinkMapper extends SinkMapper {
         this.eventGroupEnabled = Boolean.valueOf(optionHolder.validateAndGetStaticValue(OPTION_GROUP_EVENTS,
                                                                                         DEFAULT_GROUP_EVENTS));
         String delimiterValue = optionHolder.getOrCreateOption(DELIMITER, DEFAULT_DELIMITER).getValue();
-        this.delimiter = "\\t".equals(delimiterValue) ? '\t' : delimiterValue.charAt(0);
+        this.delimiter = TAB_DElIMITER_INPUT.equals(delimiterValue) ? TAB_DElIMITER : delimiterValue.charAt(0);
         headerOfData = new Object[streamDefinition.getAttributeNameArray().length];
         if (payloadTemplateBuilderMap == null) {
             dataOfEvent = new Object[streamDefinition.getAttributeNameArray().length];
